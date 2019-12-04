a64de6fee5cab5374cdf05ceba1e8365fb816e5d
Perers Tomas <tomas.perers@afconsult.com>
2019-12-04 15:10:53 +0100

    Created methods for about

---
+++ src/about.js
@@ -0,0 +1,55 @@
+'use strict';
+var About = require('../models/about');
+
+const about = {
+    add: async function (res, body) {
+        About.create({
+            name: body.name,
+            description: body.desc,
+            location: body.homeTown,
+            interest: body.interest,
+        }).then(() => {
+            return res.status(200).json({
+                message: "ok"
+            });
+        }).catch(err => {
+            return res.status(500).json({
+                message: err
+            })
+        })
+
+    },
+    update: async function (res, body) {
+        let user = await About.findOne({ name: body.name }).then((res, err) => {
+            if (err) {
+                return null;
+            }
+            console.log("i find", res)
+            return res;
+        });
+        if (user) {
+            user.name = body.name;
+            user.description = body.desc;
+            user.location = body.homeTown;
+            user.interest = body.interest;
+
+            user.save().then(() => {
+                return res.status(200).json({
+                    message: "ok"
+                });
+            }).catch(err => {
+                return res.status(500).json({
+                    message: err
+                })
+            })
+            
+        } else {
+            return res.status(500).json({
+                message: "fail"
+            })
+        }
+
+    }
+}
+
+module.exports = about;

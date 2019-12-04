35105a5777480a41897237aed58fc9865e8d628f
Tomas Perers <9654120+zarkroc@users.noreply.github.com>
2019-12-04 15:07:57 +0100

    created work history model

---
+++ models/work.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    company: { type: String, required: true },
+    description: { type: String, required: true },
+    start: { type: String, required: true },
+    stop: { type: String, required: true },
+    role: { type: String, required: true }
+});
+
+module.exports = mongoose.model('WorkPlace', schema);

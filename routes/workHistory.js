2a75c58d08f43a3de58fce66705794f45617729f
Perers Tomas <tomas.perers@afconsult.com>
2019-12-04 15:10:30 +0100

    Created routes for work history

---
+++ routes/workHistory.js
@@ -0,0 +1,29 @@
+'use strict';
+const Work = require('../models/work');
+const workHistory = require('../src/workHistory');
+
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', async function (req, res) {
+    var workPlaces = await Work.find(function (err, docs) {
+        return docs;
+    });
+    const data = {
+        data: {
+            title: "Tomas Perers | Work History",
+            workPlaces: workPlaces
+        }
+    };
+
+    res.json(data);
+});
+
+router.post('/add', //(req, res, next) => auth.checkToken(req, res, next),
+(req, res) => workHistory.add(res, req.body));
+
+router.put('/add', //(req, res, next) => auth.checkToken(req, res, next),
+(req, res) => workHistory.update(res, req.body));
+
+module.exports = router;
